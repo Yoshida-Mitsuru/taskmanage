caec62db7a0b23042c67a1559b3b6d029de72790
yomi <yoshida_mitsuru@ymail.ne.jp>
2025-01-25 21:43:03 +0900

    GroupTableDaoTest、テスト項目追加

--- src/main/java/test/GroupTableDaoTest.java
+++ src/main/java/test/GroupTableDaoTest.java
@@ -20,12 +20,14 @@ public class GroupTableDaoTest {
 	private TransactionManager trans = null;
 	private GroupUserRelationDAO relationDAO = null;
 	private GroupTableDAO target = null;
+	private GroupBean testGroup = null;
 
 	@BeforeEach
 	public void setUp() throws SQLException, IOException {
 		trans = new TransactionManager();
 		relationDAO = new GroupUserRelationDAO(trans);
 		target = new GroupTableDAO(trans);
+		testGroup = new GroupBean("hogehoge", "テスト");
 	}
 
 	@AfterEach
@@ -40,40 +42,39 @@ public class GroupTableDaoTest {
 	}
 
 	@Test
-	void 存在するデータ取得() throws Exception {
-		GroupBean expected = new GroupBean("hogehoge", "テスト");
-		assertTrue(target.create(expected));
-		GroupBean actual = target.find(expected.getId());
-		assertNotEquals(0, actual.getId());
-		assertEquals(expected, actual);
+	void 追加_自動採番() throws Exception {
+		assertTrue(target.create(testGroup));
+		assertNotEquals(0, testGroup.getId());
 	}
 
 	@Test
-	void 存在しないID() throws Exception {
+	void 取得１件_存在するデータ() throws Exception {
+		追加_自動採番();
+		GroupBean actual = target.find(testGroup.getId());
+		assertNotEquals(0, actual.getId());
+		assertEquals(testGroup, actual);
+	}
+
+	@Test
+	void 取得１件_存在しないデータ() throws Exception {
 		SQLException exception = assertThrows(SQLException.class, () -> {
 			target.find(-1);
 		});
-
-		// 例外のメッセージを確認
 		assertEquals("データが存在しません", exception.getMessage());
 	}
 
 	@Test
-	void 追加と削除_正常() throws Exception {
-		// 追加
-		GroupBean group = new GroupBean("hogehoge", "テスト");
+	void 全件取得と削除() throws Exception {
 		List<GroupBean> expected = target.findAll();
-		assertTrue(target.create(group));
-		expected.add(group);
+		expected.add(testGroup);
+		追加_自動採番();
 		List<GroupBean> actual = target.findAll();
-		assertEquals(expected.size(), actual.size());
 		assertIterableEquals(expected, actual);
 
 		// 削除
 		expected.remove(expected.size() - 1);
-		assertTrue(target.delete(group.getId()));
+		assertTrue(target.delete(testGroup.getId()));
 		actual = target.findAll();
-		assertEquals(expected.size(), actual.size());
 		assertIterableEquals(expected, actual);
 	}
 
@@ -83,14 +84,16 @@ public class GroupTableDaoTest {
 		OptionalInt maxId = expected.stream()
 				.mapToInt(GroupBean::getId)
 				.max(); // 最大値を求める
-		int id = 1;
+		int id = 0;
 		if(maxId.isPresent()) {
 			id = maxId.getAsInt()+100;
+		} else {
+			id = 1;
 		}
-		GroupBean group = new GroupBean(id, "hogehoge", "テスト");
-		assertTrue(target.create(group));
-		assertEquals(id, group.getId());
-		expected.add(group);
+		testGroup.setId(id);
+		assertTrue(target.create(testGroup));
+		assertEquals(id, testGroup.getId());
+		expected.add(testGroup);
 		List<GroupBean> actual = target.findAll();
 		assertIterableEquals(expected, actual);
 	}
@@ -98,6 +101,16 @@ public class GroupTableDaoTest {
 	@Test
 	void 削除対象なし() throws Exception {
 		assertFalse(target.delete(-1));
+	}
+
+	@Test
+	void 更新() throws Exception {
+		追加_自動採番();
+		testGroup.setName("fugafuga");
+		testGroup.setDescription("abcdefgh");
+		assertTrue(target.update(testGroup));
+		GroupBean actual = target.find(testGroup.getId());
+		assertEquals(testGroup, actual);
 	}
 
 	@Test
@@ -114,10 +127,9 @@ public class GroupTableDaoTest {
 		assertEquals(0, actual.size());
 
 		// 自動採番のリセット確認
-		GroupBean group = new GroupBean("hogehoge", "テスト");
-		assertTrue(target.create(group));
-		assertEquals(expected.size()+1, group.getId());
+		assertTrue(target.create(testGroup));
+		assertEquals(expected.size()+1, testGroup.getId());
 		actual = target.findAll();
-		assertEquals(group, actual.get(0));
+		assertEquals(testGroup, actual.get(0));
 	}
 }
